dbc2296f1ba03592831472b15e7ac0f70a6dab1d
ASquaredM <43936923+ASquaredM@users.noreply.github.com>
2021-11-26 18:14:30 +0200

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -1,35 +1,41 @@
-from flask import Flask, request, render_template
+import logging
 import os
 import random
-import redis
 import socket
 import sys
-import logging
 from datetime import datetime
 
+import redis
+from flask import Flask, render_template, request
+from opencensus.ext.azure import metrics_exporter
 # App Insights
 # DTODO: Import required libraries for App Insights
-from opencensus.ext.azure.log_exporter import AzureLogHandler
-from opencensus.ext.azure.log_exporter import AzureEventHandler
-from opencensus.ext.azure import metrics_exporter
+from opencensus.ext.azure.log_exporter import (AzureEventHandler,
+                                               AzureLogHandler)
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.stats import aggregation as aggregation_module
 from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
 from opencensus.stats import view as view_module
 from opencensus.tags import tag_map as tag_map_module
 from opencensus.trace import config_integration
-from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
-from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 stats = stats_module.stats
 view_manager = stats.view_manager
+config_integration.trace_integrations(["logging"])
+config_integration.trace_integrations(["requests"])
 
 # Logging
 # DTODO: Setup logger
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=f29139c3-9d23-4632-995b-49399ed7383b'))
+handler = AzureLogHandler(connection_string="InstrumentationKey=f29139c3-9d23-4632-995b-49399ed7383b")
+handler.setFormatter(logging.Formatter("%(message)s"))
+logger.addHandler(handler)
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=f29139c3-9d23-4632-995b-49399ed7383b'))
+logger.setLevel(logging.INFO)
 
 # Metrics
 # DTODO: Setup exporter
@@ -97,11 +103,13 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # DTODO: use tracer object to trace cat vote
-        tracer.span(name="Cats Vote")
+        with tracer.span(name="Cats Vote") as span:
+            print("Cats Vote")
         
         vote2 = r.get(button2).decode('utf-8')
         # DTODO: use tracer object to trace dog vote
-        tracer.span(name="Dogs Vote")
+        with tracer.span(name="Dogs Vote") as span:
+            print("Dogs Vote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -116,12 +124,12 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # DTODO: use logger object to log cat vote
-            logger.info("***Cat vote logged***")
+            logger.info("***Cat vote logged***", extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # DTODO: use logger object to log dog vote
-            logger.info("***Dog vote logged***")
+            logger.info("***Dog vote logged***", extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
